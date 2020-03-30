24a3ffa7a7876da32aa0564f5e579a8e70316d65
Santeri Saraluhta <santerisaraluhta@gmail.com>
2020-03-30 11:01:08 +0300

    Add basic list of scales and chords to highlight tab

--- app/src/main/java/fi/sntr/fretboard/music/Music.java
+++ app/src/main/java/fi/sntr/fretboard/music/Music.java
@@ -1,6 +1,16 @@
 package fi.sntr.fretboard.music;
 
-class Music {
+import android.util.Log;
+
+import java.util.Map;
+import java.util.LinkedHashMap;
+
+public class Music {
+
+    public Music() {
+        Log.d("Debug", "Music contructor");
+    }
+
     static final int NOTE_COUNT = 12;
 
     static final String[] NAMES_SHARP = {
@@ -10,4 +20,74 @@ class Music {
     static final String[] NAMES_FLAT = {
             "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"
     };
+
+    public static String[] INTERVALS = {
+            "1", "b2", "2", "b3", "3", "4", "b5", "5", "b6", "6", "b7", "7",
+            "8", "b9", "9", "b10", "10", "11", "b12", "12", "b13", "13"
+    };
+
+    public static Map<String, int[]> CHORDS = createChordsMap();
+
+    public static Map<String, int[]> SCALES = createScalesMap();
+
+    private static Map<String, int[]> createChordsMap() {
+        Map<String, int[]> chords = new LinkedHashMap<>();
+        add(chords, "", 0, 4, 7);
+        add(chords, "m", 0, 3, 7);
+        add(chords, "dim", 0, 3, 6);
+        add(chords, "dim7", 0, 3, 6, 9);
+        add(chords, "m7b5", 0, 3, 6, 10);
+        add(chords, "aug", 0, 4, 8);
+        add(chords, "5", 0, 7);
+        add(chords, "7", 0, 4, 7, 10);
+        add(chords, "m7", 0, 3, 7, 10);
+        add(chords, "maj7", 0, 4, 7, 11);
+        add(chords, "m/maj7", 0, 3, 7, 11);
+        add(chords, "sus4", 0, 5, 7);
+        add(chords, "sus2", 0, 2, 7);
+        add(chords, "7sus4", 0, 5, 7, 10);
+        add(chords, "7sus2", 0, 2, 7, 10);
+        add(chords, "add2", 0, 2, 4, 7);
+        add(chords, "add9", 0, 4, 7, 14);
+        add(chords, "add4", 0, 4, 5, 7);
+        add(chords, "6", 0, 4, 7, 9);
+        add(chords, "m6", 0, 3, 7, 9);
+        add(chords, "6/9", 0, 4, 7, 9, 14);
+        add(chords, "9", 0, 4, 7, 10, 14);
+        add(chords, "m9", 0, 3, 7, 10, 14);
+        add(chords, "maj9", 0, 4, 7, 11, 14);
+        add(chords, "11", 0, 4, 7, 10, 14, 17);
+        add(chords, "m11", 0, 3, 7, 10, 14, 17);
+        add(chords, "maj11", 0, 4, 7, 11, 14, 17);
+        add(chords, "13", 0, 4, 7, 10, 14, 17, 21);
+        add(chords, "m13", 0, 3, 7, 10, 14, 17, 21);
+        add(chords, "maj13", 0, 4, 7, 11, 14, 17, 21);
+        add(chords, "7#9", 0, 4, 7, 10, 15);
+        add(chords, "7b9", 0, 4, 7, 10, 13);
+        add(chords, "7#5", 0, 4, 8, 10);
+        add(chords, "7b5", 0, 4, 6, 10);
+        return chords;
+    }
+
+    private static Map<String, int[]> createScalesMap() {
+        Map<String, int[]> scales = new LinkedHashMap<>();
+        add(scales, "Chromatic", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11);
+        add(scales, "Major", 0, 2, 4, 5, 7, 9, 11);
+        add(scales, "Dorian", 0, 2, 3, 5, 7, 9, 10);
+        add(scales, "Phrygian", 0, 1, 3, 5, 7, 8, 10);
+        add(scales, "Lydian", 0, 2, 4, 6, 7, 9, 11);
+        add(scales, "Mixolydian", 0, 2, 4, 5, 7, 9, 10);
+        add(scales, "Aeolian", 0, 2, 3, 5, 7, 8, 10);
+        add(scales, "Locrian", 0, 1, 3, 5, 6, 8, 10);
+        add(scales, "Minor", 0, 2, 3, 5, 7, 8, 10);
+        add(scales, "Harmonic minor", 0, 2, 3, 5, 7, 8, 11);
+        add(scales, "Melodic minor", 0, 2, 3, 5, 7, 8, 9, 10, 11);
+        add(scales, "Pentatonic", 0, 2, 4, 7, 9);
+        add(scales, "Minor pentatonic", 0, 3, 5, 7, 10);
+        return scales;
+    }
+
+    private static void add(Map<String, int[]> map, String name, int... notes) {
+        map.put(name, notes);
+    }
 }
--- app/src/main/java/fi/sntr/fretboard/tabs/HighlightFragment.java
+++ app/src/main/java/fi/sntr/fretboard/tabs/HighlightFragment.java
@@ -6,14 +6,23 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView.Adapter;
+import androidx.recyclerview.widget.RecyclerView.ViewHolder;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Map;
 
 import fi.sntr.fretboard.MainActivity;
 import fi.sntr.fretboard.R;
 import fi.sntr.fretboard.music.Instrument;
+import fi.sntr.fretboard.music.Music;
 
 public class HighlightFragment extends Fragment {
 
@@ -25,6 +34,22 @@ public class HighlightFragment extends Fragment {
         super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.tab_highlight, container, false);
 
+        RecyclerView chordsRecycler = view.findViewById(R.id.chords_view);
+        HighlightViewAdapter mChordsAdapter = new HighlightViewAdapter(Music.CHORDS);
+        chordsRecycler.setAdapter(mChordsAdapter);
+
+        LinearLayoutManager chordsLayoutManager = new LinearLayoutManager(getContext());
+        chordsRecycler.setLayoutManager(chordsLayoutManager);
+
+        RecyclerView scalesRecycler = view.findViewById(R.id.scales_view);
+        HighlightViewAdapter mScalesAdapter = new HighlightViewAdapter(Music.SCALES);
+        scalesRecycler.setAdapter(mScalesAdapter);
+
+        LinearLayoutManager scalesLayoutManager = new LinearLayoutManager(getContext());
+        scalesRecycler.setLayoutManager(scalesLayoutManager);
+        Log.d("Debug", "Music.SCALES " + Music.SCALES.size());
+        Log.d("Debug", "Music.CHORDS " + Music.CHORDS.size());
+
         return view;
     }
 
@@ -33,6 +58,45 @@ public class HighlightFragment extends Fragment {
         super.onAttach(context);
         if(context instanceof MainActivity) {
             mInstrument = ((MainActivity) context).instrument;
+        }
+    }
+
+    class HighlightViewAdapter extends Adapter<HighlightViewHolder> {
+
+        private Map<String, int[]> mHighlights;
+
+        public HighlightViewAdapter(Map<String, int[]> highlights) {
+            mHighlights = highlights;
+            Log.d("mHighlights", "mHighlights " + mHighlights.size());
+        }
+
+        @NonNull
+        @Override
+        public HighlightViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.fragment_highlight_item, parent, false);
+            return new HighlightViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull HighlightViewHolder holder, int position) {
+            holder.highlightNameText.setText(mHighlights.keySet().toArray()[position].toString());
+            Log.d("Deubg", mHighlights.keySet().toArray()[position].toString());
+        }
+
+        @Override
+        public int getItemCount() {
+            return mHighlights.size();
+        }
+    }
+
+    private static class HighlightViewHolder extends ViewHolder {
+        TextView highlightNameText;
+
+        public HighlightViewHolder(@NonNull View view) {
+            super(view);
+            Log.d("Deubg","Create hl view");
+            highlightNameText = view.findViewById(R.id.highlight_name_text);
         }
     }
 }
